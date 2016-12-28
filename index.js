08014a93aa2d3bb2151020da86f908fb5ff5bff1
mitroofann <mitroofann@gmail.com>
2016-12-28 15:48:59 +0500

    Stable

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+/**
+ * @file
+ * Этот модуль нужен для упрощения синхронной работы и выстроения нужных функций для выполнения в цепочку методов
+ * @author mitroofann <mitroofann@gmail.com>
+ * @copyright Michael Firsov 2016
+ * @version 1.0.0
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2016 mitroofann
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 
 const defaultMethods = {
 	catch: _catch,
@@ -5,8 +35,15 @@ const defaultMethods = {
 	wait: _wait
 };
 const util           = require('util');
-const TIME           = require('time');
+//const TIME           = require('mitroofann-datetime');
 
+/**
+ * Функция принимает функцию-конструктор и методы которые нужно добавить в прототип
+ * Возвращает Конструктор с добавленными методами в прототип, которые можно выстраивать в цепочку
+ * @param {constructor} Model Конструктор
+ * @param {{string: function}} methods Методы которые нужно добавить в прототип конструктору
+ * @returns {function}
+ */
 function createAPI (Model, methods)
 {
 	if (typeof Model !== 'function')
@@ -14,9 +51,19 @@ function createAPI (Model, methods)
 		throw new Error(`Model expected to be a constructor.`);
 	}
 
-	if (!methods || typeof methods !== 'object')
+	if (typeof methods !== 'object')
 	{
 		throw new Error(`An argument must be an object containing function-methods.`);
+	}
+	else
+	{
+		for (let method in methods)
+		{
+			if (methods.hasOwnProperty(method) && typeof methods[method] !== 'function')
+			{
+				throw new Error(`An argument must be an object containing function-methods.`);
+			}
+		}
 	}
 
 	_addMethods(Model, methods);
@@ -26,6 +73,13 @@ function createAPI (Model, methods)
 	return Model;
 }
 
+/**
+ * Служебная функция
+ * Оборачивает конструктор и добавляет в него собственные служебные методы
+ * @param {constructor} Model Конструктор
+ * @returns {function}
+ * @private
+ */
 function _wrapAPI (Model)
 {
 	function ModelWrapped (...rest)
@@ -42,6 +96,11 @@ function _wrapAPI (Model)
 	return ModelWrapped;
 }
 
+/**
+ * Служебный конструктор для обертки функции
+ * @constructor
+ * @private
+ */
 function API()
 {
 	Object.defineProperty(
@@ -76,6 +135,13 @@ function API()
 	);
 }
 
+/**
+ * Служебная функция
+ * Функция добавляет методы в прототип конструктору
+ * @param {constructor} Model функция конструктор, в которую нужно добавить обернутые методы
+ * @param {{string: function}} methods Методы, которые нужно добавить в прототип
+ * @private
+ */
 function _addMethods (Model, methods)
 {
 	methods = Object.assign(methods, defaultMethods);
@@ -84,29 +150,40 @@ function _addMethods (Model, methods)
 	{
 		if (methods.hasOwnProperty(name))
 		{
-			if (typeof methods[name] === 'function')
-			{
-				_addMethod(
-					Model,
-					name,
-					methods[name]
-				);
-			}
-			else
-			{
-				throw new Error(`Each Method must be a function. Method ${name} is not a function.`);
-			}
+			_addMethod(
+				Model,
+				name,
+				methods[name]
+			);
 		}
 	}
 }
 
+/**
+ * Служебная функция
+ * Добавляет метод в прототип конструктору
+ * @param {constructor} Model Конструктор, в который нужно добавить метод
+ * @param {string} name Наименование метода
+ * @param {function} method Функция-метод
+ * @private
+ */
 function _addMethod (Model, name, method)
 {
 	Model.prototype[name] = _wrap(name, method);
 }
 
+/**
+ * Служебная функция
+ * Оборачивает метод который нужно чейнить
+ * @param {string} name Наименование метода
+ * @param {function} method Функция-метод
+ * @returns {function}
+ * @private
+ */
 function _wrap (name, method)
 {
+	return _wrapper;
+
 	function _wrapper (...params)
 	{
 		_chain.call(
@@ -118,10 +195,16 @@ function _wrap (name, method)
 
 		return this;
 	}
-
-	return _wrapper;
 }
 
+/**
+ * Служебная функция
+ * Функция вызывается при вызове обернутого метода из прототипа
+ * @param {string} name Наименование метода
+ * @param {function} method Функция-метод
+ * @param {*[]} params
+ * @private
+ */
 function _chain (name, method, params)
 {
 	const next = _callee.bind(
@@ -146,6 +229,14 @@ function _chain (name, method, params)
 	}
 }
 
+/**
+ * Служебная функция
+ * Функция вызывает метод из цепочки
+ * @param method
+ * @param params
+ * @param args
+ * @private
+ */
 function _callee (method, params, ...args)
 {
 	const called = {
@@ -160,6 +251,14 @@ function _callee (method, params, ...args)
 	);
 }
 
+/**
+ * Служебная функция
+ * Функция, которая передается в следующий метод, которую можно вызвать с результатом выполнения для передачи управления следующей функции
+ * @param {{called: boolean}} called
+ * @param params
+ * @returns {*}
+ * @private
+ */
 function _resolve (called, ...params)
 {
 	if (false === called.value)
@@ -173,6 +272,14 @@ function _resolve (called, ...params)
 	}
 }
 
+/**
+ * Служебная функция
+ * Функция, которая передается в следующий метод, которую можно вызвать с ошибокй для передачи управления функции обработки ошибок
+ * @param {{called: boolean}} called
+ * @param params
+ * @returns {*}
+ * @private
+ */
 function _reject (called, ...params)
 {
 	let err = params[0] || new Error('Rejected without an error');
@@ -200,6 +307,13 @@ function _reject (called, ...params)
 	);
 }
 
+/**
+ * Служебная функция
+ * При возникновении ошибки в выполнении одной из функций из цепочки ищет близжайшую функцию-ловушку для ошибок и передает управление ей, либо бросает ошибку в лучае отсутствия такого метода
+ * @param {Error} err
+ * @throws {Error}
+ * @private
+ */
 function _rejectHelper (err)
 {
 	if (this.____currentCallback !== 0 && this.____callbacks.length > this.____currentCallback)
@@ -223,6 +337,12 @@ function _rejectHelper (err)
 	throw new Error(`Use "catch" method to catch errors thrown`);
 }
 
+/**
+ * Служебная функция
+ * Вызывает следующую функцию из цепочки при выполнении предыдущей
+ * @param params
+ * @private
+ */
 function _next (...params)
 {
 	this.____currentCallback = 0;
@@ -265,6 +385,15 @@ function _next (...params)
 	}
 }
 
+/**
+ * Функция добавляется в прототип конструктору
+ * Позволяет ловить ошибки возникшие в цепочке и передавать их обработку в пользовательскую функцию
+ * @param {function} resolve
+ * @param {function} reject
+ * @param {function} [callee] Функция, в которую передастся ошибка в случае ее возникновения
+ * @param {Error} err
+ * @private
+ */
 function _catch (resolve, reject, callee, err)
 {
 	if (typeof callee !== 'function')
@@ -275,6 +404,15 @@ function _catch (resolve, reject, callee, err)
 	callee.call(this, resolve, reject, err);
 }
 
+/**
+ * Функция добавляется в прототип конструктору
+ * Позволяет вызвать в цепочке любую функцию
+ * @param {function} resolve
+ * @param {function} reject
+ * @param {function} [callee] Функция, которую нужно вызвать в очереди цепочки
+ * @param rest
+ * @private
+ */
 function _then (resolve, reject, callee, ...rest)
 {
 	if (typeof callee !== 'function')
@@ -285,9 +423,18 @@ function _then (resolve, reject, callee, ...rest)
 	callee.call(this, resolve, reject, ...rest);
 }
 
+/**
+ * Функция добавляется в прототип конструктору
+ * Инициализирует задержку перед выполнением следующей функции из цепочки
+ * @param {function} resolve
+ * @param {function} reject
+ * @param {number} [ms] Количество миллисекунд которое необходимо ждать, по умолчанию - рандомное число от 1000 до 60000
+ * @param rest
+ * @private
+ */
 function _wait (resolve, reject, ms, ...rest)
 {
-	setTimeout(resolve, ms || TIME.MS.random(1000, 60000), ...rest);
+	setTimeout(resolve, ms || /*TIME.MS.random(1000, 60000)*/1, ...rest);
 }
 
 module.exports = createAPI;
